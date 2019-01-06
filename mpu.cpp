bc794d2caf248e94389fa4404c326222cdb728da
nm47 <nielsmoeller9@gmail.com>
2019-01-05 22:20:56 -0600

    Implemented a simple Kalman filter to smooth mpu value

--- mpu.cpp
+++ mpu.cpp
@@ -1,6 +1,14 @@
 #include "mpu.hpp"
+#include <SimpleKalmanFilter.h>
 
 #define OUTPUT_READABLE_ACCELGYRO
+
+//Kalman parameters
+float e_mea = 4;  //measurement uncertainty
+float e_est = 4;  //estimation uncertainty
+float proccessVariance = .01; //how fast the value will change (0.001 - .1)
+
+SimpleKalmanFilter kalman(e_mea,e_est,proccessVariance);
 
  void mpu::setup(){
     Wire.begin();
@@ -12,5 +20,10 @@
 }
 
 bool mpu::tick(){
-  Serial.println(gyro.getRotationX());
+  int16_t rawValue = gyro.getRotationX();
+  double estimated = kalman.updateEstimate(rawValue);
+  Serial.print(rawValue);
+  Serial.print(",");
+  Serial.print(estimated);
+  Serial.println(",");
 }
