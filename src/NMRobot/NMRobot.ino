5b3d96a3cd6e123c962f740610b200de649e3bfa
nm47 <nielsmoeller9@gmail.com>
2019-01-08 13:18:23 -0600

    Include all libraries used to fix problems with arduino IDE

--- src/NMRobot/NMRobot.ino
+++ src/NMRobot/NMRobot.ino
@@ -1,15 +1,15 @@
 //#include <digitalWriteFast.h>
 
+#include "JJ_MPU6050_DMP_6Axis.h"    // |
+#include <Wire.h>                    // |
+#include <I2Cdev.h>                  // | Arduino (stupidly) demands implicit references to all files used in a project in the ino file...
+#include "Mpu.hpp"                   // |
+#include "NMbot.hpp"                 // |
+
 #define DIR_PIN_L 9
 #define PUL_PIN_L 8
 #define DIR_PIN_R 6
 #define PUL_PIN_R 7
-
-#include <Wire.h>
-#include <I2Cdev.h>
-
-#include "mpu.hpp"
-#include "NMbot.hpp"
 
 NMbot bot;
 
