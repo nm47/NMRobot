05770d263ebf95ec978ac574c2b97812700613b0
Niels Moeller <Nielsmoeller9@gmail.com>
2019-09-11 17:45:08 -0500

    moving back to github

--- src/mpu.cpp
+++ src/mpu.cpp
@@ -3,7 +3,10 @@
 #define OUTPUT_READABLE_QUATERNION
 uint8_t fifoBuffer[18];
 
- void mpu::setup(){
+double mpu::angle = 0.;
+bool mpu::dmpOnline = false;
+
+void mpu::setup(){
     dmpOnline = false;
 
     Wire.begin();
@@ -15,7 +18,8 @@ uint8_t fifoBuffer[18];
     gyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
     gyro.setDLPFMode(MPU6050_DLPF_BW_20);  //10,20,42,98,188
     gyro.setRate(4);   // 0=1khz 1=500hz, 2=333hz, 3=250hz 4=200hz
-    gyro.setSleepEnabled(false);
+    //gyro.setSleepEnabled(false);
+    
     Serial.println(F("Testing device connections..."));
     Serial.println(gyro.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
     delay(2000);
@@ -33,30 +37,27 @@ uint8_t fifoBuffer[18];
       Serial.print(dmpStatus);
       Serial.println(F(")"));
     }
+   delay(1000);
    gyro.resetFIFO();
 }
 
 bool mpu::tick(){
   fifocount = gyro.getFIFOCount();
-  //packetSize = gyro.dmpGetFIFOPacketSize(); 
-  if (fifocount < 18){
-    return false;
-  }
-  /*else if (packetSize < fifocount)
-  {
-    gyro.resetFIFO();
-    return false;
-  }*/
+  if (fifocount >= 18){
+    if (fifocount > 18){
+      gyro.resetFIFO();
+      return false;
+    }
+    gyro.getFIFOBytes(fifoBuffer, 16);
+    gyro.dmpGetQuaternion(&q, fifoBuffer);
   
-  gyro.getFIFOBytes(fifoBuffer, 16);
-  gyro.dmpGetQuaternion(&q, fifoBuffer);
-  gyro.resetFIFO();
-  angle = atan2(2.0*(q.y*q.z + q.w*q.x), q.w*q.w - q.x*q.x - q.y*q.y + q.z*q.z) * 57.2957795; //we want yaw, other formulas for reference
-
-  Serial.println(angle);
-  // yaw = atan2(2.0*(q.y*q.z + q.w*q.x), q.w*q.w - q.x*q.x - q.y*q.y + q.z*q.z);
-  // pitch = asin(-2.0*(q.x*q.z - q.w*q.y));
-  // roll = atan2(2.0*(q.x*q.y + q.w*q.z), q.w*q.w + q.x*q.x - q.y*q.y - q.z*q.z);
-
-  return true;
+    
+   //angle = atan2(2.0*(q.y*q.z + q.w*q.x), q.w*q.w - q.x*q.x - q.y*q.y + q.z*q.z);
+    angle =  asin(-2.0*(q.x*q.z - q.w*q.y));
+   //roll = atan2(2.0*(q.x*q.y + q.w*q.z), q.w*q.w + q.x*q.x - q.y*q.y - q.z*q.z);
+  // Serial.print(angle);
+    gyro.resetFIFO();
+    return true;
+  }
+  return false;
 }
