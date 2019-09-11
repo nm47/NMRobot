eb99fe4bc384193efd017f3cf539efa29cb1935e
Niels Moeller <Nielsmoeller9@gmail.com>
2019-09-11 16:49:49 -0500

    Clean up code
    No more g variables, added functions for motor speed

--- src/NMbot.hpp
+++ src/NMbot.hpp
@@ -3,12 +3,9 @@
 
 class NMbot{
     public:
-      int16_t ay;
-      int16_t gy;
-      int16_t my;
-  
       void setup();
       void loop();
+      void SetMotorSpeed(int motor, double mspeed);
       void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency);
-
+      void CalculateSpeed();
 };
