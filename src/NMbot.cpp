3075a55187836d3bab9efbc18b0eb7c2c21b5715
Niels Moeller <Nielsmoeller9@gmail.com>
2019-09-11 16:25:04 -0500

    Perform speed calculations with PID controller

--- src/NMbot.cpp
+++ src/NMbot.cpp
@@ -1,20 +1,53 @@
 #include "NMbot.hpp"
 #include "MPU6050.h"
 #include "mpu.hpp"
+#include <PID_v1.h>
 
-mpu motion;
-
+mpu gyro;
+double kp = 11;
+double ki = 5;
+double kd = 1;
+double setPoint = 0;
+double currentSpeed = 0;
+double pidOutputSpeed;
+double input;
+PID pid = PID (&input, &pidOutputSpeed, &setPoint, kp, ki, kd, DIRECT);
 void NMbot::setup(){
     Serial.begin(115200);
-    motion.setup();
-    startTimer(TC1, 0, TC3_IRQn, 600); //TC1 channel 0, the IRQ for that channel and the desired frequency
+    gyro.setup();
+    
+    pid.SetOutputLimits(-1000,1000);
+    startTimer(TC1, 0, TC3_IRQn, 1000); //TC1 channel 0, the IRQ for that channel and the desired frequency
+    startTimer(TC1, 1, TC4_IRQn, 1000);
 }
 
 void NMbot::loop(){
     if(motion.tick()){
     }
 }
+void NMbot::CalculateSpeed(){
+    input = gyro.angle;
+    pid.Compute();
+    currentSpeed = pidOutputSpeed;
 
+    Serial.print(",");
+    Serial.println(pidOutputSpeed);
+
+    SetMotorSpeed(0,pidOutputSpeed);
+    pidOutputSpeed = map(mspeed, -300,300, -1000,1000);
+    pidOutputSpeed = constrain(mspeed, -1000,1000);
+    
+}
+
+void NMbot::SetMotorSpeed(int motor, double mSpeed){  //0 is left, 1 is right, motors are mounted in opposite directions, therefore rotate opposite dirs.
+  
+  if (motor == 0){
+    startTimer(TC1, 0, TC3_IRQn, &pidOutputSpeed);
+  }
+  else {
+    startTimer(TC1, 1, TC4_IRQn, pidOutputSpeed);
+  }
+}
 
 //Initializes SAM3X8E wave generator at 50% duty cycle, triggers interupt on rising edge of the wave
 void NMbot::startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
