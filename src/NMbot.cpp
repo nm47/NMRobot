cda8ed6e2ebcae9d90328503da3c5fc2a755ee31
Niels Moeller <Nielsmoeller9@gmail.com>
2020-08-13 15:49:45 -0500

    Improve Documentation

--- src/NMbot.cpp
+++ src/NMbot.cpp
@@ -8,10 +8,12 @@
 #define DIR_PIN_R 6
 #define PUL_PIN_R 7
 
+//Uses an MPU6050 Accel/Gyro.
 mpu motion;
 
+//Microstepping at 1/32 to allow fine movement of the motors
 int microstep = 32;
-double kp = 23.7;
+double kp = 3.7;
 double ki = 0;
 double kd = .4;
 double setPoint = -1.22; //The balancing point is not an exact 90 degrees.
@@ -19,44 +21,52 @@ double currentSpeed = 0;
 double pidOutputSpeed;
 double input;
 
+//Initialize PID to control motor pulse speed
 PID pid = PID(&input, &pidOutputSpeed, &setPoint, kp, ki, kd, DIRECT);
 
 void NMbot::setup(){
+    //Begin Serial comms and initialize the motion processor.
     Serial.begin(115200);
     motion.setup();
- 
-    pid.SetOutputLimits(-1000,1000);
+    
+    //Limit the PID to outputs of +/- 500.
+    pid.SetOutputLimits(-500,500);
+    //Begin PID, options are auto or manual, auto=PID controls output
+    //manual = PID effectively off, you must calculate correct speed.
     pid.SetMode(AUTOMATIC);
 }
 
 void NMbot::loop(){
     if(motion.tick()){
-        motion.tick();
         Serial.println(motion.angle);
     }
 }
 
 void NMbot::CalculateSpeed(){
+    //Input to the PID is the current angle of the accelerometer.
     input = motion.angle;
-    
+    //Compute the speed we need to move to keep the robot upright (minimize error between motion.angle and setpoint).
     pid.Compute();
-
+    //Set our desired speed to the PID output.
     currentSpeed = pidOutputSpeed;
     
     SetMotorSpeed(0,currentSpeed);
     SetMotorSpeed(1,currentSpeed);
 }
-void NMbot::SetMotorSpeed(int motor, double mSpeed){  //0 is left, 1 is right, motors are mounted in opposite directions, therefore rotate opposite dirs.
+void NMbot::SetMotorSpeed(int motor, double mSpeed){  
+  //0 is left, 1 is right, motors are mounted in opposite directions, therefore rotate opposite dirs.
+  // If we want to move forward, set the motor direction to forwards.  
   if(mSpeed > 0){
     if(motor == 0)digitalWrite(DIR_PIN_L, LOW);
     else digitalWrite(DIR_PIN_R, HIGH);
   }
+  // If we want to move backwards, set the motor direction to backwards.  
   else if (mSpeed < 0){
     if(motor == 0) digitalWrite(DIR_PIN_L, HIGH );
     else digitalWrite(DIR_PIN_R, LOW);
   }
     
-    
+  // Start software interrupt to pulse motor at mSpeed
   if (motor == 0)
     startTimer(TC1, 0, TC3_IRQn, abs(mSpeed *microstep)); //Microstepping at 1/32
   else
